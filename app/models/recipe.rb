6caad87a46d68c4228b14be8b64d8bc14948e381
Sean Morrison & Nick Bernard <student@epicodus.com>
2014-08-22 10:22:23 -0700

    Adds Tag class and spec.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,5 +2,7 @@ class Recipe < ActiveRecord::Base
   validates :name, :presence => true
   validates :instructions, :presence => true
   validates :date, :presence => true
-  validates_numericality_of :rating, less_than: 6, greater_than: 0
+  validates_numericality_of :rating, less_than: 6, greater_than: 0, allow_blank: true
+
+  has_and_belongs_to_many :tags
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -9,4 +9,13 @@ describe Recipe do
     is_less_than(6).
     is_greater_than(0)
   end
+  it "has and belongs to many tags" do
+    new_recipe = Recipe.create(:name => "Ramen", :instructions => "Boil water", :date => "2014-06-19")
+    tag1 = Tag.create(:name => "Casual Dining")
+    tag2 = Tag.create(:name => "Do Not Serve, Ever")
+    new_recipe.tags << tag1
+    new_recipe.tags << tag2
+    expect(new_recipe.tags).to eq [tag1, tag2]
+  end
 end
+
